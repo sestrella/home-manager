8ac0ca692746161c05f503e14feaf26ac7c1b4ef
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2026-09-05 09:38:25 -0500

    Increase test coverage for validate-files-enabled-validator-set

--- tests/modules/files/validate-files-enabled-validator-set.nix
+++ tests/modules/files/validate-files-enabled-validator-set.nix
@@ -1,12 +1,20 @@
 { config, pkgs, ... }:
 
 {
-  home.file."test-file" = {
-    text = "test content";
-    validate = {
-      enabled = true;
-      validator = { source }: "echo 'validated ${source}'";
-      # validator = { source }: "echo 'validated ${source}'";
+  home.file = {
+    test-file-1 = {
+      text = "test content 1";
+      validate = {
+        enabled = true;
+        validator = { source }: "echo 'validated ${source}'";
+      };
+    };
+    test-file-2 = {
+      text = "test content 2";
+      validate = {
+        enabled = true;
+        validator = { source }: "echo 'validated ${source}'";
+      };
     };
   };
 
@@ -17,6 +25,11 @@
     ''
       substitute ${activationScript} $TMPDIR/activate --subst-var TMPDIR
       chmod +x $TMPDIR/activate
-      $TMPDIR/activate
+      assertFileIsExecutable "$TMPDIR/activate"
+
+      $TMPDIR/activate &> $TMPDIR/output
+      assertFileExists "$TMPDIR/output"
+      assertFileRegex "$TMPDIR/output" "validated /nix/store/.*-hm_testfile1"
+      assertFileRegex "$TMPDIR/output" "validated /nix/store/.*-hm_testfile2"
     '';
 }
